764c790cd2390f47b2e25342f29adcfec59f4fa4
Alex Cooksey <aecooksey2651@gmail.com>
2022-05-16 17:06:51 -0400

    Completed assignment

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,11 @@
 function selectionSort(arr) {
-  // type your code here
+  let sorted = [];
+  while (arr.length > 0) {
+    const min = Math.min(...arr)
+    arr.splice(arr.indexOf(min), 1)
+    sorted.push(min)
+  }
+  return sorted
 }
 
 if (require.main === module) {
@@ -7,6 +13,12 @@ if (require.main === module) {
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
 
+  console.log("Expecting: [2, 4, 6, 8, 9");
+  console.log("=>", selectionSort([4, 2, 9, 6, 8]))
+
+  console.log("Expecting: [-20, -3, 25, 37, 1042]")
+  console.log("=>", selectionSort([37, 1042, -20, 25, -3]))
+  
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
@@ -21,3 +33,7 @@ module.exports = selectionSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// IN OWN WORDS: find lowest value, then insert at correct spot in new sorted array
+// 1) find minimum value in input Array
+// 2) find appropriate spot in new array and insert this value
+
